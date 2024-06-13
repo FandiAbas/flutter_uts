66e7930b2180287dd8943949b0ccdabc0c796015
FandiAbas <fandiabas@00gmail.com>
2024-06-13 23:25:45 +0800

    git init flutter_uts

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_uts/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Tugas UTS/Fandi Abas'),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.logout),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Icon(Icons.person),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 3,
-                children: List.generate(9, (index) {
-                  return Container(
-                    margin: EdgeInsets.all(8.0),
-                    color: Color.fromARGB(255, 228, 75, 152),
-                  );
-                }),
-              ),
-            ),
-            ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.blue,
-              ),
-              title: Text('Fandi'),             
-              subtitle: Text('Abas'),                     
-              trailing: Icon(Icons.remove_circle_rounded),
-            ),
-            ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Color.fromARGB(255, 211, 66, 66),
-              ),
-              title: Text('Fandi'),
-              subtitle: Text('Abas'),
-              trailing: Icon(Icons.remove_circle_rounded),
-            ),
-          ],
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const HomePage(),
     );
   }
 }
